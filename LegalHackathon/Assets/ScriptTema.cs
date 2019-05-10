17c8f8c6a34f4d9e2f8573f0133e84b31697e60e
Broly090 <broly090@gmail.com>
2019-05-10 20:42:22 +0200

    Versión 1.2
    Añadido panel de temas e hilos
    Añadida función para recoger conversaciones desde servidor

---
+++ LegalHackathon/Assets/ScriptTema.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTema : MonoBehaviour
+{
+    string nameTema = "";
+    private bool abierto = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DameDatitos("Juridico");
+        MostrarOcultarHilosTema(abierto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * Funcion DameDatitos()
+     * 
+     * Se encargara de dar nombre al boton para mostrar los hilos más recientes 
+     * 
+     * */
+
+    public void DameDatitos(string nombreTema) { this.name = nombreTema; nameTema = nombreTema; }
+
+    public void MostrarOcultarHilosTema(bool abrir)
+    {
+        GameObject[] hilos = GameObject.FindGameObjectsWithTag("contenedorHilos");
+
+        foreach (GameObject hilo in hilos)
+        {
+            if (hilo.name.Contains(this.name))
+                hilo.SetActive(abrir);
+        }
+    }
+
+
+
+}
---
+++ LegalHackathon/Assets/Scripts/BackEnd.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackEnd : MonoBehaviour
+{
+
+    private string tema, hilo, url, conversacion;
+    private TextAsset conversacionText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecogerHilo("Penal", "Art.245.2CP");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    /*
+     * 
+     * Función encargada de recoger hilos (conversaciones) desde servidor
+     * 
+     * 
+     * */
+    public void RecogerHilo (string tema, string hilo)
+    {
+
+        url = "http://46.183.118.202:6969/Legalhackathon/" + tema + "/" + hilo + ".txt";
+
+        StartCoroutine(CorrutinaRecogerHilo());
+
+    }
+
+
+    /*
+     * 
+     * Función encargada de recoger hilos (conversaciones) desde servidor
+     * 
+     * 
+     * */
+    IEnumerator CorrutinaRecogerHilo ()
+    {
+        using (var www = new WWW(url))
+        {
+            yield return www;
+
+            conversacion = System.Text.Encoding.UTF7.GetString(www.bytes);
+
+        }
+
+        Debug.Log(conversacion);
+    }
+
+
+
+
+}
